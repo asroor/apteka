c3bd1b0d8381c39145230f40b97d2396f8e89bbd
asroor <asrorgithub@gmail.com>
2023-03-01 23:17:36 +0500

    last

--- Js/app.js
+++ Js/app.js
@@ -1,7 +1,19 @@
 let mediaBars = document.querySelector('.bars');
+let bars = document.querySelector('.bars');
+let close = document.querySelector('.close-x');
+let SitePanel = document.querySelector('.SitePanel');
+let sidebar = document.querySelector('.body-sidebar');
+let katalog = document.querySelector('.katalog');
 
-mediaBars.addEventListener('click', () => {
-    mediaBars.classList.toggle('bars-active')
+katalog.addEventListener('click', () => {
+    sidebar.classList.toggle('body-sidebar-active')
+})
+
+bars.addEventListener('click', () => {
+    SitePanel.classList.add('SitePanel-active')
+})
+close.addEventListener('click', () => {
+    SitePanel.classList.remove('SitePanel-active')
 })
 
 if (Swiper) {
@@ -22,20 +34,16 @@ if (Swiper) {
 }
 
 if (Swiper) {
-    var swiper = new Swiper(".mySwiper3", {
-        spaceBetween: 10,
-        slidesPerView: 4,
-        freeMode: true,
-        watchSlidesProgress: true,
-    });
-    var swiper2 = new Swiper(".mySwiper2", {
-        spaceBetween: 10,
+    var swiper = new Swiper(".mySwiper-product", {
+        spaceBetween: 30,
+        effect: "fade",
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
-        thumbs: {
-            swiper: swiper,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
         },
     });
 }
